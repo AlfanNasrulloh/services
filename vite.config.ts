1e7cfb93bc21cd69f582aa7c93557c0770e249bb
AlfanNasrulloh <fanyoulgaming01@gmail.com>
2025-06-19 13:07:50 +0700

    set ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        cors: true,
+        host: 'localhost',
+        port: 5173,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
